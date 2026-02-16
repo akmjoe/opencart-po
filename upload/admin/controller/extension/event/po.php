dcd929564821f7de6c34a22565f2258506a3feb8
Joe Rothrock <rothrockindustries@gmail.com>
2026-02-16 15:25:37 -0500

    Removed unnecessary code from simple_html_dom

--- upload/admin/controller/extension/event/po.php
+++ upload/admin/controller/extension/event/po.php
@@ -12,18 +12,6 @@ class controllerExtensionEventPo extends Controller {
                 $data['text_po'] = $this->language->get('text_po');
                 
                 $data['header'] .= $this->load->view('extension/module/po_edit', $data);
-                
-                return;
-		// add PO number field
-		$html = new simple_html_dom();
-        $html->load($output, $lowercase = true, $stripRN = false, $defaultBRText = DEFAULT_BR_TEXT);
-		if($html->find('div#tab-payment',0)) {
-			$html->find('div#tab-payment', 0)->find('div.form-group', -1)->outertext .= 
-				'<div class="form-group"><label class="col-sm-2 control-label" for="po-number">'.$this->language->get('text_po').
-				'</label><div class="col-sm-10"><input type="text" name="po_number" id="input-payment-po-number" class="form-control" value="'.
-				$po.'"></div></div>';
-		}
-		$output = $html->save();
 	}
 	
 	public function save(&$route, &$data) {
